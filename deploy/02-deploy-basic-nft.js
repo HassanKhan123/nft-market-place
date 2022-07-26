6f5a046c9bd9898aa0130e346cccd8f6ae777f8e
HassanKhan123 <hk2602809@gmail.com>
2022-07-26 14:25:16 +0500

    added: tests

---
+++ deploy/02-deploy-basic-nft.js
@@ -0,0 +1,41 @@
+const { network } = require("hardhat");
+const {
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config");
+const { ETHERSCAN_API_KEY } = require("../secret");
+const { verify } = require("../utils/verify");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+  log("----------------------------------------------------");
+  const args = [];
+  const basicNft = await deploy("BasicNft", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  });
+
+  //   const basicNftTwo = await deploy("BasicNftTwo", {
+  //     from: deployer,
+  //     args: args,
+  //     log: true,
+  //     waitConfirmations: waitBlockConfirmations,
+  //   });
+
+  // Verify the deployment
+  if (!developmentChains.includes(network.name) && ETHERSCAN_API_KEY) {
+    log("Verifying...");
+    await verify(basicNft.address, args);
+    // await verify(basicNftTwo.address, args);
+  }
+  log("----------------------------------------------------");
+};
+
+module.exports.tags = ["all", "basicNft"];
