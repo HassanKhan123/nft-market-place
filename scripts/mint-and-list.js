6a8da695815c56feda0e99f975bd647ea5fb203a
HassanKhan123 <hk2602809@gmail.com>
2022-07-27 11:29:12 +0500

    added: scripts

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,5 @@
 const { ethers, network } = require("hardhat");
+const { moveBlock, moveBlocks } = require("./move-blocks");
 // const { moveBlocks } = require("../utils/move-blocks");
 
 const PRICE = ethers.utils.parseEther("0.1");
@@ -24,10 +25,10 @@ async function mintAndList() {
   const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE);
   await tx.wait(1);
   console.log("NFT Listed!");
-  //   if (network.config.chainId == 31337) {
-  //     // Moralis has a hard time if you move more than 1 at once!
-  //     await moveBlocks(1, (sleepAmount = 1000));
-  //   }
+  if (network.config.chainId == 31337) {
+    // Moralis has a hard time if you move more than 1 at once!
+    await moveBlocks(2, (sleepAmount = 1000));
+  }
 }
 
 mintAndList()
